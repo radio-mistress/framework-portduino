3bc17a48de591ec2f99ba2276ff24af4cb8ef843
mverch67 <manuel.verch@gmx.de>
2024-12-09 11:40:07 +0100

    fix crash in printf

--- cores/portduino/PortduinoPrint.cpp
+++ cores/portduino/PortduinoPrint.cpp
@@ -6,11 +6,12 @@
 #define MAX_STR_LEN 256
 
 size_t Print::printf(const char *format, ...) {
-  char buf[MAX_STR_LEN]; // FIXME, this burns a lot of stack, but on
+    char buf[MAX_STR_LEN]; // FIXME, this burns a lot of stack, but on
                                 // Linux that is fine, TBD on MyNewt
-  va_list args;
-  va_start(args, format);
-  vsnprintf(buf, sizeof(buf), format, args);
-  write(buf);
-  va_end(args);
+    va_list args;
+    va_start(args, format);
+    size_t n = vsnprintf(buf, sizeof(buf), format, args);
+    write(buf);
+    va_end(args);
+    return n;
 }
